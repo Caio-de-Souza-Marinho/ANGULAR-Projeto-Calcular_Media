7bb05e275dc242c5e812bc50d5fb178679f99ed6
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-24 19:45:49 -0300

    Adicionando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TabelaComponent
   ],
   imports: [
     BrowserModule,
