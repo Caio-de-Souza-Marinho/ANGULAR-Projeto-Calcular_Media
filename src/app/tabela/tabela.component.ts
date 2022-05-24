d347b3f0ee9555d8039fe7539b1fd5e5f84d4087
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-24 20:33:52 -0300

    Adicionando o componente da tabela, variáveis com as notas e diretiva ngIf

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabelaComponent implements OnInit {
 
+  codigoRMA: number = 4156;
+  nome: string = "Caio";
+  nota1: number = 10;
+  nota2: number = 10;
+  nota3: number = 3;
+  nota4: number = 5;
+  media: number = (this.nota1+this.nota2+this.nota3+this.nota4)/4;
+  data: string = "2021-07-01";
+  imagem: string = 'assets/img/caiofoto.jpg';
+
   constructor() { }
 
   ngOnInit(): void {
