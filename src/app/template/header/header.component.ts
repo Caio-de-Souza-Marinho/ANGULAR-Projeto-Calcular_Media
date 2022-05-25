58900ef2e6226a90be2521808901765d303ca815
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-24 21:00:43 -0300

    Fazendo mudanças no heager

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  logo: string = "assets/img/logo_horacio.png.png"
+
   constructor() { }
 
   ngOnInit(): void {
